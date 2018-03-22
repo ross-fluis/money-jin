809cf621f00d5a9c549609454dd6eb174834baab
Luis <luis.gregson@korelogic.co.uk>
2018-03-22 19:54:15 +0000

    add linting and prettier

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import React from 'react'
 
 export default class App extends React.Component {
   render() {
-    return <h1>Hello worlds</h1>;
+    return <h1>Hello worlds</h1>
   }
 }
