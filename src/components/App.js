55bf75c76bb3d1f6e04df099a1af251cc63dcb0e
Ross <rssfltchr@gmail.com>
2018-03-24 16:20:44 +0000

    Added whoami api

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,21 @@
 import React from 'react'
+import Monzo from '../services/monzo'
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+
+    // init service
+    this.monzo = new Monzo()
+  }
+
+  async componentWillMount() {
+    // init empty state to avoid null errors before the request has finished
+    this.setState({})
+    this.setState(await this.monzo.whoAmI())
+  }
+
   render() {
-    return <h1>Hello worlds</h1>
+    return <h1>You are {this.state.authenticated ? '' : 'not'} logged in</h1>
   }
 }
